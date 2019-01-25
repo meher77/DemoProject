9220f2ed44c98e473ec227cbbfba974c49904ba3
admin <admin@MEHER7>
2019-01-25 13:41:15 +0530

    First Commit

---
+++ AA/src/AA.java
@@ -0,0 +1,7 @@
+
+public class AA {
+
+	public static void main(String[] args) {
+		System.out.println("hii");
+	}
+}
---
+++ AA/src/com/locator/Anything.java
@@ -0,0 +1,13 @@
+package com.locator;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Anything {
+	public static void main(String args[])
+	{
+		System.setProperty("webdriver.chrome.driver","H:\\Seleinium Data\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+}
